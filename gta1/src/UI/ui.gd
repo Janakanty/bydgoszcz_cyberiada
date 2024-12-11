24eee013e46d5fdc67b45cd228b2af1d1bf0c8fe
MateuszLagocki1994 <mlagocki@wp.pl>
2024-12-11 18:56:16 +0100

    v. 0.2

--- gta1/src/UI/ui.gd
+++ gta1/src/UI/ui.gd
@@ -1,7 +1,7 @@
 extends Control
-
+var bonus_points: int = 0
 func _ready() -> void:
 	set_points(0000)
 	
 func set_points(points: int):
-	$Points.text = str(points) 
+	$Points.text = str(points + bonus_points) 
--- gta1/src/gameplay/gameplay.gd
+++ gta1/src/gameplay/gameplay.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
+const BONUS = preload("res://src/Bonus/bonus.tscn")
 var player_possition:int
+var state = "over"
 
 func _ready() -> void:
 	$Camera2D.connect("die", game_over)
+	$Bonus.connect("collected",add_points)
+	random_bonus()
+	random_bonus()
+	random_bonus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,10 +24,24 @@ func game_over():
 	print("game over")
 
 func camera_state() -> void:
-	var state = "over"
+	if $Player.global_position.y - $Camera2D.global_position.y < 0:
+		state = "over"
+	else:
+		state = "under"
 	
 	match state:
 		"over":
 			$Camera2D.global_position.y = $Player.global_position.y
 		"under":
-			pass
+			$Camera2D.global_position.y -= 1
+
+func add_points():
+	$UI/UI.bonus_points = $UI/UI.bonus_points + 1000000
+	print("Bonus Points")
+	
+func random_bonus():
+	var b = BONUS.instantiate()
+	b.global_position.x = randi_range(0,1100)
+	b.global_position.y = randi_range(-1200,0)
+	add_child(b)
+	
